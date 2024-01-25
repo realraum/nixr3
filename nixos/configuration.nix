3afd4eb5829653ca8d0765e86a1852009d5ebbd2
Maciej Krüger <mkg20001@gmail.com>
2024-01-25 21:17:16 +0100

    put common config into common module

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -18,57 +18,11 @@
 
     # Import your generated (nixos-generate-config) hardware configuration
     #./hardware-configuration.nix
+    ../modules/common.nix
     ../modules/qemu-base.nix
   ];
 
-  nixpkgs = {
-    hostPlatform = "x86_64-linux";
-
-    # You can add overlays here
-    overlays = [
-      # If you want to use overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
-    ];
-    # Configure your nixpkgs instance
-    config = {
-      # Disable if you don't want unfree packages
-      allowUnfree = true;
-    };
-  };
-
-  # This will add each flake input as a registry
-  # To make nix3 commands consistent with your flake
-  nix.registry = (lib.mapAttrs (_: flake: {inherit flake;})) ((lib.filterAttrs (_: lib.isType "flake")) inputs);
-
-  # This will additionally add your inputs to the system's legacy channels
-  # Making legacy nix commands consistent as well, awesome!
-  nix.nixPath = ["/etc/nix/path"];
-  environment.etc =
-    lib.mapAttrs'
-    (name: value: {
-      name = "nix/path/${name}";
-      value.source = value.flake;
-    })
-    config.nix.registry;
-
-  nix.settings = {
-    # Enable flakes and new 'nix' command
-    experimental-features = "nix-command flakes";
-    # Deduplicate and optimize nix store
-    auto-optimise-store = true;
-  };
-
-  # testing future stuff
-  networking.useNetworkd = true;
-  boot.initrd.systemd.enable = true;
-  networking.nftables.enable = true;
+  nixpkgs.hostPlatform = "x86_64-linux";
 
   networking.useDHCP = false;
   networking.interfaces.ens3.useDHCP = true;
@@ -81,48 +35,9 @@
   # TODO: This is just an example, be sure to use whatever bootloader you prefer
   boot.loader.systemd-boot.enable = true;
 
-  environment.systemPackages = with pkgs; [
-    dstat
-    tcpdump
-  ];
-
-  programs.screen = {
-    enable = true;
-    screenrc = ''
-      defscrollback 10000
-      startup_message off
-      '';
-  };
-
-  programs.htop = {
-    enable = true;
-    settings = {
-      hide_userland_threads = true;
-    };
-  };
-
   users.users = {
     root = {
       initialPassword = "test123";
-
-      # TODO: You can set an initial password for your user.
-      # If you do, you can skip setting a root password by passing '--no-root-passwd' to nixos-install.
-      # Be sure to change it (using passwd) after rebooting!
-      openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILazlApx8QjprxmwHLNGGV0NIYsElzDrFocfvLfV6pVs gebi@realraum"
-      ];
-    };
-  };
-
-  # This setups a SSH server. Very important if you're setting up a headless system.
-  # Feel free to remove if you don't need it.
-  services.openssh = {
-    enable = true;
-    settings = {
-      # Forbid root login through SSH.
-      PermitRootLogin = "without-password";
-      # Use keys only. Remove if you want to SSH using password (not recommended)
-      PasswordAuthentication = false;
     };
   };
 
