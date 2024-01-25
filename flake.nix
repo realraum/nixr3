9fe7290b4014d85320a86f07c914f090d3cd42ad
Maciej Krüger <mkg20001@gmail.com>
2024-01-25 21:18:26 +0100

    cleanup

--- flake.nix
+++ flake.nix
@@ -5,12 +5,8 @@
     # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
-    # Home manager
-    #home-manager.url = "github:nix-community/home-manager/release-23.05";
-    #home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
     # TODO: Add any other flake you might need
-    # hardware.url = "github:nixos/nixos-hardware";
+    hardware.url = "github:nixos/nixos-hardware";
 
     # Shameless plug: looking for a way to nixify your themes and make
     # everything match nicely? Try nix-colors!
@@ -20,7 +16,6 @@
   outputs = {
     self,
     nixpkgs,
-    #home-manager,
     ...
   } @ inputs: let
     inherit (self) outputs;
@@ -32,20 +27,8 @@
       iot = nixpkgs.lib.nixosSystem {
         specialArgs = {inherit inputs outputs;};
         # > Our main nixos configuration file <
-        modules = [./nixos/configuration.nix];
+        modules = [./iot/configuration.nix];
       };
     };
-
-    # Standalone home-manager configuration entrypoint
-    # Available through 'home-manager --flake .#your-username@your-hostname'
-    #homeConfigurations = {
-    #  # FIXME replace with your username@hostname
-    #  "your-username@your-hostname" = home-manager.lib.homeManagerConfiguration {
-    #    pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-    #    extraSpecialArgs = {inherit inputs outputs;};
-    #    # > Our main home-manager configuration file <
-    #    modules = [./home-manager/home.nix];
-    #  };
-    #};
   };
 }
