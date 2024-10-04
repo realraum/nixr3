1fa627e376567015ff5bc9a6c323b7a2d009edd3
Maciej Krüger <mkg20001@gmail.com>
2024-10-04 03:17:26 +0200

    modules: fix nixos-container

--- modules/nixos-container.nix
+++ modules/nixos-container.nix
@@ -2,6 +2,7 @@
   # Use systemd-resolved inside the container
   # Workaround for bug https://github.com/NixOS/nixpkgs/issues/162686
   networking.useHostResolvConf = lib.mkForce false;
+  boot.initrd.systemd.enable = lib.mkForce false;
 
   services.resolved.enable = true;
 }
